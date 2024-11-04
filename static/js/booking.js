e75ccdb5f3b2d9a98f034adff7895f4fddf5ff48
Eduard <edwardamirov@gmail.com>
2024-11-04 21:48:24 +0500

    5

--- static/js/booking.js
+++ static/js/booking.js
@@ -123,6 +123,10 @@ let selectedService = null;
 let selectedMaster = null;
 let selectedServicePrice = null;
 
+window.onload = function() {
+        document.getElementById('auth-modal').style.display = 'block';
+    };
+
 // Функция для переключения вкладок
 function openTab(event, tabId) {
     const tabs = document.getElementsByClassName('tab-content');
